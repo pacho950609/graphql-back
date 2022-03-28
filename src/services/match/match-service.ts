0dd0dc4c7952381c542817c6349cdee6fa5f8661
Francisco Ricaurte <franciscoricaurte@Franciscos-MacBook-Pro.local>
2022-03-28 07:21:58 -0500

    feat(match): validate set date

--- src/services/match/match-service.ts
+++ src/services/match/match-service.ts
@@ -1,6 +1,7 @@
 import { Match } from 'entities/Match';
 import { GameSet } from 'entities/Set';
 import { Connection } from 'typeorm';
+import _ from 'lodash';
 
 interface GameSetI {
     firstPlayerPoints: number;
@@ -13,15 +14,25 @@ export const validateSet = (set: GameSetI) => {
         set.secondPlayerPoints >= 10 &&
         Math.abs(set.firstPlayerPoints - set.secondPlayerPoints) !== 2
     ) {
-        throw new Error('Points difference must be two');
+        throw new Error(`Points difference must be two in set ${set.setNumber}`);
     } else if (set.firstPlayerPoints > set.secondPlayerPoints) {
-        if (set.firstPlayerPoints < 11 || set.firstPlayerPoints > 11) throw new Error('Set ends at 11 points');
+        if (set.firstPlayerPoints < 11 || set.firstPlayerPoints > 11) throw new Error(`Set must end at 11 points in set in set ${set.setNumber}`);
     } else if (set.secondPlayerPoints > set.firstPlayerPoints) {
-        if (set.secondPlayerPoints < 11 || set.secondPlayerPoints > 11) throw new Error('Set ends at 11 points');
+        if (set.secondPlayerPoints < 11 || set.secondPlayerPoints > 11) throw new Error(`Set must end at 11 points in set in set ${set.setNumber}`);
     } else if (set.firstPlayerPoints === set.secondPlayerPoints) {
-        throw new Error('There must be a winner in the set');
+        throw new Error(`There must be a winner in the set ${set.setNumber}`);
     }
 };
+
+export const validateSets = (sets: GameSetI[]) => {
+    _.range(sets.length).map(setNumber => {
+        const set = sets.find(gameSet => gameSet.setNumber === setNumber + 1)
+        if(!set){
+            throw new Error(`Doesn't exist a set with number ${setNumber + 1}`)
+        } 
+        validateSet(set);
+    })
+}
 
 export const createMatch = async (
     params: {
@@ -32,7 +43,12 @@ export const createMatch = async (
     connection: Connection = null,
 ) => {
     const { sets, firstPlayerId, secondPlayerId } = params;
-    sets.map((set) => validateSet(set));
+    validateSets(sets);
+
+
+    if (sets.length > 5) {
+        throw new Error('Max sets number is 5');
+    }
 
     const setsWins = sets.reduce(
         (prev, curr) => {
@@ -49,6 +65,14 @@ export const createMatch = async (
         },
         { first: 0, second: 0 },
     );
+
+    if (setsWins.first > 3 || setsWins.second > 3) {
+        throw new Error('Game ends after win 3 sets');
+    }
+
+    if (setsWins.first !== 3 && setsWins.second !== 3) {
+        throw new Error('There must be a winner');
+    }
 
     const winnerPlayerId = setsWins.first > setsWins.second ? firstPlayerId : secondPlayerId;
     const loserPlayerId = setsWins.first > setsWins.second ? secondPlayerId : firstPlayerId;
